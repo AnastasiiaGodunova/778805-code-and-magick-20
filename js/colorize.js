c70b581780e8bb2dfa62c4ab0ea5af04e61db218
AnastasiiaGodunova <icq95@mail.ru>
2020-06-23 17:28:17 +0300

    Получение данных с сервера

--- js/colorize.js
+++ js/colorize.js
@@ -4,8 +4,11 @@
   var setupWizard = window.setup.querySelector('.setup-wizard');
   var wizardCoat = setupWizard.querySelector('.wizard-coat');
   var wizardCoatColor = window.setup.querySelector('input[name=coat-color]');
+  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var wizardEyes = setupWizard.querySelector('.wizard-eyes');
   var wizardEyesColor = window.setup.querySelector('input[name=eyes-color]');
+  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
   var wizardFireball = window.setup.querySelector('.setup-fireball-wrap');
   var wizardFireballColor = window.setup.querySelector('input[name=fireball-color]');
   var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -25,11 +28,11 @@
   };
 
   wizardCoat.addEventListener('click', function () {
-    getColor(wizardCoat, window.wizardRender.coatColor, wizardCoatColor, 'fill', 'coat');
+    getColor(wizardCoat, coatColor, wizardCoatColor, 'fill', 'coat');
   });
 
   wizardEyes.addEventListener('click', function () {
-    getColor(wizardEyes, window.wizardRender.eyesColor, wizardEyesColor, 'fill', 'eyes');
+    getColor(wizardEyes, eyesColor, wizardEyesColor, 'fill', 'eyes');
   });
 
   wizardFireball.addEventListener('click', function () {
