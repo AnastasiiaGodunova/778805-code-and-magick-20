c7b3ad6a16dcac78ba62765dcbb267c26fd6792d
AnastasiiaGodunova <icq95@mail.ru>
2020-07-01 13:13:32 +0300

    Похожие маги

--- js/colorize.js
+++ js/colorize.js
@@ -1,21 +1,26 @@
 'use strict';
 
 (function () {
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
   var setupWizard = window.setup.querySelector('.setup-wizard');
   var wizardCoat = setupWizard.querySelector('.wizard-coat');
   var wizardCoatColor = window.setup.querySelector('input[name=coat-color]');
-  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var wizardEyes = setupWizard.querySelector('.wizard-eyes');
   var wizardEyesColor = window.setup.querySelector('input[name=eyes-color]');
-  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
   var wizardFireball = window.setup.querySelector('.setup-fireball-wrap');
   var wizardFireballColor = window.setup.querySelector('input[name=fireball-color]');
-  var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var counters = {
     coat: 1,
     eyes: 1,
     fireball: 1
+  };
+  var wizard = {
+    onCoatChange: function (color) {},
+    onEyesChange: function (color) {}
   };
 
   var getColor = function (obj, arr, input, cssTxt, count) {
@@ -28,14 +33,20 @@
   };
 
   wizardCoat.addEventListener('click', function () {
-    getColor(wizardCoat, coatColor, wizardCoatColor, 'fill', 'coat');
+    getColor(wizardCoat, COAT_COLORS, wizardCoatColor, 'fill', 'coat');
+    var newColor = this.style.fill;
+    wizard.onCoatChange(newColor);
   });
 
   wizardEyes.addEventListener('click', function () {
-    getColor(wizardEyes, eyesColor, wizardEyesColor, 'fill', 'eyes');
+    getColor(wizardEyes, EYES_COLORS, wizardEyesColor, 'fill', 'eyes');
+    var newColor = wizardEyes.style.fill;
+    wizard.onEyesChange(newColor);
   });
 
   wizardFireball.addEventListener('click', function () {
-    getColor(wizardFireball, fireballColor, wizardFireballColor, 'background-color', 'fireball');
+    getColor(wizardFireball, FIREBALL_COLORS, wizardFireballColor, 'background-color', 'fireball');
   });
+
+  window.wizard = wizard;
 })();
